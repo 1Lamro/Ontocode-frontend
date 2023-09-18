e1e033e06fd1028806223167532e7d5c1f21c9a4
Mokhamed <gari142@mail.ru>
2023-09-18 17:07:00 +0300

    rgrega

--- frontendProject/src/App.tsx
+++ frontendProject/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.module.css'
-
+import PricePage from './components/pages/PricePage/Price'
+import { Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
-    <>
-
-    </>
+    <div>
+<Routes>
+  <Route path='/Price' element={<PricePage />} />
+</Routes>
+    </div>
   )
 }
 
