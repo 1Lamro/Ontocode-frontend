195e3982c1893fc50c245750e4d132d6007a2521
Ali <stealth4119@gmail.com>
2023-09-19 10:41:57 +0300

    сделал хидер

--- frontendProject/src/App.tsx
+++ frontendProject/src/App.tsx
@@ -1,6 +1,7 @@
 import styles from './App.module.css'
 import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header/Header'
+import Home from './components/pages/HomePage/Home'
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
       <div className={styles.app}>
         <Header />
         <Routes>
-
+          <Route path='/' element={<Home/>}/>
         </Routes>
       </div>
 
--- frontendProject/src/components/Header/Header.tsx
+++ frontendProject/src/components/Header/Header.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import { Link } from 'react-router-dom'
+import styles from './Header.module.css'
+import logo from '../../../public/logo.svg'
+import triangle from './nav_triangle.svg'
+import search from './search.png'
 
 const Header = () => {
     return (
-        <div>
-            <div><img src="" alt="" /></div>
-            <nav>
-                123 
+        <div className={styles.headerContainer}>
+            <nav className={styles.navigation}>
+                <Link to='/' ><img src={logo} alt="logo" className={styles.logo} /></Link>
+                <Link to='/resources' className={styles.zero}><p>Resources</p><img src={triangle} alt="" /></Link>
+                <Link to='/price' className={styles.zero}><p>Pricing</p><img src={triangle} alt="" /></Link>
+                <Link to='/for-teams' className={styles.zero}><p>For Teams</p><img src={triangle} alt="" /></Link>
             </nav>
+            <div className={styles.loginContainer}>
+                <img src={search} className={styles.search}/>
+                <Link to='/login' className={styles.loginButton}><p>Log In</p></Link>
+            </div>
         </div>
     );
 };
