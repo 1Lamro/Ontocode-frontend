f90b28b9b073343dbef3c72720717f0d432c06e9
talhache <adam_ibragimov_1996@mail.ru>
2023-09-20 22:53:07 +0300

    Болх ца бо де дакъ

--- frontendProject/src/components/pages/ChatPage/components/sideBar.tsx
+++ frontendProject/src/components/pages/ChatPage/components/sideBar.tsx
@@ -1,7 +1,13 @@
 import { useEffect, useState } from "react";
 import styles from '../chat.module.css'
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../../../app/store";
 
 const sideBar = ({ socket }) => {
+
+    const token = useSelector((state: RootState) => state.application.token);
+    const User = useSelector((state: RootState) => state.user.users);
+    console.log(token)
 
     const [users, setUsers] = useState([]);
 
@@ -15,14 +21,12 @@ const sideBar = ({ socket }) => {
             t.user === value.user && t.socketID === value.socketID
         ))
     )
-
+    console.log(User, filteredList);
     return (
         <div className={styles.sidebar}>
             <h4 className={styles.headerS}>Users</h4>
             <ul className={styles.users}>
-                {filteredList.map((element) => (
-                    <li key={element.socketID}>{element.user}</li>
-                ))}
+                {token !== undefined ? User.map(item => <li key={item._id}>{item.username}</li>) : filteredList.map(element => <li key={element.socketID}>{element.name}</li>)}
             </ul>
         </div>
     );
