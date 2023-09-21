bf3cc15848ad83dd88b53d047eb5a54dc3dbeffe
talhache <adam_ibragimov_1996@mail.ru>
2023-09-21 10:43:24 +0300

    Chat fix

--- frontendProject/src/components/pages/ChatPage/components/sideBar.tsx
+++ frontendProject/src/components/pages/ChatPage/components/sideBar.tsx
@@ -2,16 +2,42 @@ import { useEffect, useState } from "react";
 import styles from '../chat.module.css'
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../../../app/store";
+import { oneUser } from "../../../../features/userSlice";
 
 const sideBar = ({ socket }) => {
 
-    const token = useSelector((state: RootState) => state.application.token);
-    const User = useSelector((state: RootState) => state.user.users);
-    console.log(token)
+    function parseJWT(tokenUser) {
+        if (typeof tokenUser !== "string") {
+          // Обработка ошибки или возврат значения по умолчанию
+          return null;
+        }
+        const base64Url = tokenUser.split(".")[1];
+        const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+        const jsonPayload = decodeURIComponent(
+          atob(base64)
+            .split("")
+            .map(function (c) {
+              return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+            })
+            .join("")
+        );
+        return JSON.parse(jsonPayload);
+      }
+      
+      const token = useSelector((state: RootState) => state.application.token);
+      const User = useSelector((state: RootState) => state.user.users);
+
+      const dispatch = useDispatch();
+
+
+
+      const ownid = parseJWT(token);
+      console.log(ownid)
 
     const [users, setUsers] = useState([]);
 
     useEffect(() => {
+        dispatch(oneUser(ownid.userId))
         socket.on('responseNewUser', (data) => setUsers(data))
 
     }, [socket, users]);
@@ -21,12 +47,12 @@ const sideBar = ({ socket }) => {
             t.user === value.user && t.socketID === value.socketID
         ))
     )
-    console.log(User, filteredList);
+    console.log(User);
     return (
         <div className={styles.sidebar}>
             <h4 className={styles.headerS}>Users</h4>
             <ul className={styles.users}>
-                {token !== undefined ? User.map(item => <li key={item._id}>{item.username}</li>) : filteredList.map(element => <li key={element.socketID}>{element.name}</li>)}
+                {token !== undefined ?  <li >{User.username}</li> : filteredList.map(element => <li key={element.name}>{element.name}</li>)}
             </ul>
         </div>
     );
