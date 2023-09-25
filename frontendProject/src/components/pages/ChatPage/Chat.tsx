1f5d3ae27eb0bc55c68fe823b4c96095025858f0
talhache <adam_ibragimov_1996@mail.ru>
2023-09-25 17:48:58 +0300

    fix chat: scroll, deleted messages, get users

--- frontendProject/src/components/pages/ChatPage/Chat.tsx
+++ frontendProject/src/components/pages/ChatPage/Chat.tsx
@@ -3,30 +3,35 @@ import Body from "./components/body";
 import MessageBlock from "./components/messageBlock";
 import styles from './chat.module.css'
 import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../../app/store";
+import { getMessage } from "../../../features/chatSlice";
 
 function Chat({ socket }) {
+  const dispatch = useDispatch()
 
-  const [messages, setMessages] = useState([]);
+  // const [messages, setMessages] = useState([]);
   const [status, setStatus] = useState('');
+  const messages = useSelector((state: RootState) => state.chat.chat)
 
   useEffect(() => {
-    socket.on('response', (data) => setMessages([...messages, data]))
+    socket.on('response', (data) => dispatch(getMessage()))
   }, [socket, messages]);
 
   useEffect(() => {
+    dispatch(getMessage())
     socket.on('responseTyping', (data) => {
       setStatus(data)
       setTimeout(() => setStatus(''), 1000)
     })
 
-  }, [socket])
-
+  }, [socket, dispatch])
 
   return (
     <div className={styles.chat}>
       <SideBar socket={socket} />
       <main className={styles.main}>
-        <Body messages={messages} status={status} socket={socket} />
+        <Body status={status} socket={socket} />
         <MessageBlock socket={socket} />
 
       </main>
