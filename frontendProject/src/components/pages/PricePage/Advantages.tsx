9d5e90612258eace71d142424685d76dced5b579
Ali <stealth4119@gmail.com>
2023-09-29 04:17:43 +0300

    add verstka

--- frontendProject/src/components/pages/PricePage/Advantages.tsx
+++ frontendProject/src/components/pages/PricePage/Advantages.tsx
@@ -7,7 +7,7 @@ const Advantages = () => {
             <table className={styles.table}>
                 <tbody>
                     <tr>
-                        <td>Features</td>
+                        <td>Привилегии</td>
                         <td>Basic</td>
                         <td>Plus</td>
                         <td>Pro</td>
@@ -16,55 +16,37 @@ const Advantages = () => {
                         <td colSpan="4">Начни обучаться программированию</td>
                     </tr>
                     <tr>
-                        <td>Basic&nbsp;access&nbsp;to&nbsp;free&nbsp;courses&nbsp;</td>
+                        <td className={styles.whiteTd}>Базовый доступ к бесплатным курсам</td>
+                        <td className={styles.whiteTd}><img src={check}/></td>
+                        <td className={styles.whiteTd}><img src={check}/></td>
+                        <td className={styles.whiteTd}><img src={check}/></td>
+                    </tr>
+                    <tr>
+                        <td>Помощь сообщества</td>
                         <td><img src={check}/></td>
                         <td><img src={check}/></td>
                         <td><img src={check}/></td>
                     </tr>
                     <tr>
-                        <td>Community&nbsp;support&nbsp;</td>
-                        <td><img src={check}/></td>
-                        <td><img src={check}/></td>
-                        <td><img src={check}/></td>
+                        <td className={styles.whiteTd}>Обучающие ресурсы</td>
+                        <td className={styles.whiteTd}><img src={check}/></td>
+                        <td className={styles.whiteTd}><img src={check}/></td>
+                        <td className={styles.whiteTd}><img src={check}/></td>
                     </tr>
                     <tr>
-                        <td>Learning&nbsp;resources&nbsp;</td>
-                        <td><img src={check}/></td>
-                        <td><img src={check}/></td>
-                        <td><img src={check}/></td>
-                    </tr>
-                    <tr>
-                        <td>Practice&nbsp;on&nbsp;our&nbsp;mobile&nbsp;app&nbsp;</td>
-                        <td><img src={check}/></td>
-                        <td><img src={check}/></td>
-                        <td><img src={check}/></td>
-                    </tr>
-                    <tr>
-                        <td>All&nbsp;courses&nbsp;</td>
+                        <td>Все курсы</td>
+                        <td>-</td>
                         <td>-</td>
                         <td><img src={check}/></td>
-                        <td><img src={check}/></td>
                     </tr>
                     <tr>
-                        <td>Quizzes&nbsp;</td>
-                        <td>-</td>
-                        <td><img src={check}/></td>
-                        <td><img src={check}/></td>
+                        <td colSpan="4">Развитие навыков</td>
                     </tr>
                     <tr>
-                        <td>Personalized&nbsp;practice&nbsp;</td>
-                        <td>-</td>
-                        <td><img src={check}/></td>
-                        <td><img src={check}/></td>
-                    </tr>
-                    <tr>
-                        <td colSpan="4">Build your skills</td>
-                    </tr>
-                    <tr>
-                        <td>Skill&nbsp;paths&nbsp;</td>
-                        <td>-</td>
-                        <td><img src={check}/></td>
-                        <td><img src={check}/></td>
+                        <td className={styles.whiteTd}>Skill&nbsp;paths&nbsp;</td>
+                        <td className={styles.whiteTd}>-</td>
+                        <td className={styles.whiteTd}><img src={check}/></td>
+                        <td className={styles.whiteTd}><img src={check}/></td>
                     </tr>
                     <tr>
                         <td>Real-world&nbsp;projects&nbsp;</td>
@@ -73,31 +55,31 @@ const Advantages = () => {
                         <td><img src={check}/></td>
                     </tr>
                     <tr>
-                        <td>Certificates&nbsp;of&nbsp;completion&nbsp;</td>
+                        <td className={styles.whiteTd}>Сертификат об окончании</td>
+                        <td className={styles.whiteTd}>-</td>
+                        <td className={styles.whiteTd}><img src={check}/></td>
+                        <td className={styles.whiteTd}><img src={check}/></td>
+                    </tr>
+                    <tr>
+                        <td colSpan="4">Устройство на работу</td>
+                    </tr>
+                    <tr>
+                        <td className={styles.whiteTd}>Карьерная неделя</td>
+                        <td className={styles.whiteTd}>-</td>
+                        <td className={styles.whiteTd}>-</td>
+                        <td className={styles.whiteTd}><img src={check}/></td>
+                    </tr>
+                    <tr>
+                        <td>Профессиональная сертификация</td>
                         <td>-</td>
-                        <td><img src={check}/></td>
+                        <td>-</td>
                         <td><img src={check}/></td>
                     </tr>
                     <tr>
-                        <td colSpan="4">Land a job</td>
-                    </tr>
-                    <tr>
-                        <td>Career&nbsp;paths&nbsp;</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td><img src={check}/></td>
-                    </tr>
-                    <tr>
-                        <td>Professional&nbsp;certifications&nbsp;</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td><img src={check}/></td>
-                    </tr>
-                    <tr>
-                        <td>Technical&nbsp;interview&nbsp;prep&nbsp;</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td><img src={check}/></td>
+                        <td className={styles.whiteTd}>Technical&nbsp;interview&nbsp;prep&nbsp;</td>
+                        <td className={styles.whiteTd}>-</td>
+                        <td className={styles.whiteTd}>-</td>
+                        <td className={styles.whiteTd}><img src={check}/></td>
                     </tr>
                     <tr>
                         <td>Career&nbsp;services&nbsp;</td>
@@ -106,24 +88,15 @@ const Advantages = () => {
                         <td><img src={check}/></td>
                     </tr>
                     <tr>
-                        <td>Code&nbsp;challenges&nbsp;</td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td><img src={check}/></td>
+                        <td className={styles.whiteTd}>Задачи с кодом</td>
+                        <td className={styles.whiteTd}>-</td>
+                        <td className={styles.whiteTd}>-</td>
+                        <td className={styles.whiteTd}><img src={check}/></td>
                     </tr>
                     <tr>
-                        <td>Assessments&nbsp;</td>
+                        <td>Ассесмент</td>
                         <td>-</td>
                         <td>-</td>
-                        <td><img src={check}/></td>
-                    </tr>
-                    <tr>
-                        <td colSpan="4">Additional features</td>
-                    </tr>
-                    <tr>
-                        <td>Priority&nbsp;customer&nbsp;support&nbsp;</td>
-                        <td>-</td>
-                        <td><img src={check}/></td>
                         <td><img src={check}/></td>
                     </tr>
                 </tbody>
