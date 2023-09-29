04f0c57faae5dabee948c7a922da051e58935f8f
talhache <adam_ibragimov_1996@mail.ru>
2023-09-29 14:18:18 +0300

    ready chat

--- frontendProject/src/components/pages/ChatPage/ChatForm.tsx
+++ frontendProject/src/components/pages/ChatPage/ChatForm.tsx
@@ -1,29 +1,29 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import styles from './chat.module.css'
+// import { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import styles from './chat.module.css'
 
-const ChatForm = ({ socket }) => {
+// const ChatForm = ({ socket }) => {
 
-    const navigate = useNavigate();
-    const [user, setUser] = useState('')
+//     const navigate = useNavigate();
+//     const [user, setUser] = useState('')
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        localStorage.setItem('user', user);
-        socket.emit('newUser', { user, soketID: socket.id })
-        navigate('/chat')
-    }
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         localStorage.setItem('user', user);
+//         socket.emit('newUser', { user, soketID: socket.id })
+//         navigate('/chat')
+//     }
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit} className={styles.container}>
-                <h2>Войти в чат</h2>
-                <label htmlFor='user'> </label>
-                <input type="text" id='user' value={user} className={styles.userInput} onChange={(e) => setUser(e.target.value)} />
-                <button type='submit' className={styles.homeBtn}>Войти</button>
-            </form>
-        </div>
-    );
-};
+//     return (
+//         <div>
+//             <form onSubmit={handleSubmit} className={styles.container}>
+//                 <h2>Войти в чат</h2>
+//                 <label htmlFor='user'> </label>
+//                 <input type="text" id='user' value={user} className={styles.userInput} onChange={(e) => setUser(e.target.value)} />
+//                 <button type='submit' className={styles.homeBtn}>Войти</button>
+//             </form>
+//         </div>
+//     );
+// };
 
-export default ChatForm;
+// export default ChatForm;
